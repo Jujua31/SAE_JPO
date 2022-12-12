37c798c09091e6865b23e7c60d3d27b36490e469
bap-haudebourg <118758890+bap-haudebourg@users.noreply.github.com>
2022-12-12 17:46:51 +0100

    Update script.js

--- Facile/script.js
+++ Facile/script.js
@@ -280,6 +280,7 @@ next.addEventListener("click", () => {
       selected = "";
       evaluatebutton = false;
     } else {
+      alert('Vous avez obtenu un résultat de '+ bonnerep +"/"+ (bonnerep+mauvaisrep));
       window.location.replace("../accueil.html"); 
     }
     
